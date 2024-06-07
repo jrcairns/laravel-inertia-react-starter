c89d1bf5ae3de2f2be68d582903329ff7df4a554
Justin Cairns <justincairnsuk@gmail.com>
2024-06-06 22:47:54 -0600

    remove unused packages

--- resources/js/pages/profile/partials/update-password-form.tsx
+++ resources/js/pages/profile/partials/update-password-form.tsx
@@ -2,7 +2,6 @@ import InputError from '@/components/input-error';
 import { Button } from '@/components/ui/button';
 import { Input } from '@/components/ui/input';
 import { Label } from '@/components/ui/label';
-import { Transition } from '@headlessui/react';
 import { useForm } from '@inertiajs/react';
 import { FormEventHandler, useRef } from 'react';
 
--- resources/js/pages/profile/partials/update-profile-information-form.tsx
+++ resources/js/pages/profile/partials/update-profile-information-form.tsx
@@ -3,7 +3,6 @@ import { Button } from '@/components/ui/button';
 import { Input } from '@/components/ui/input';
 import { Label } from '@/components/ui/label';
 import { PageProps } from '@/types';
-import { Transition } from '@headlessui/react';
 import { Link, useForm, usePage } from '@inertiajs/react';
 import { FormEventHandler } from 'react';
 

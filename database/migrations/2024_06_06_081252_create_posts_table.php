d9748ac5d5cd48d03de2e14d6424a3892f76330d
Justin Cairns <justincairnsuk@gmail.com>
2024-06-08 21:36:18 -0600

    update migrations

--- database/migrations/2024_06_06_081252_create_posts_table.php
+++ database/migrations/2024_06_06_081252_create_posts_table.php
@@ -13,6 +13,7 @@ return new class extends Migration
     {
         Schema::create('posts', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('user_id');
             $table->text('title');
             $table->text('content');
             $table->timestamps();
